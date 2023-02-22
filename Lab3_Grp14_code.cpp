d78eaae7d2efd23c82c2de87de5ea75f996d0270
Albert <albert.gagalac@obf.ateneo.edu>
2023-02-23 05:33:11 +0800

    got push to sort of work + changes to menu

--- Lab3_Grp14_code.cpp
+++ Lab3_Grp14_code.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "Lab3_Grp14_struct.hpp"
-#include <cstring>
+#include <string>
+
 using namespace std;
 
 int main(void){
@@ -8,36 +9,56 @@ int main(void){
     IntNode initialNode; 
     IntNode item;
 
-    string func;
+    int func;
     int exit=0;
+    int y;
 
-    do {
-        cout<<"Enter function: 1-Push 2-Pop";
+    while (exit == 0) {
+        cout<<"Enter function: 1-Push 2-Pop\n";
         cin>>func;
-
-        switch (func){
-            case 1: 
+        if (func == 1)
+        {
             cout << "enter value: " << endl;
             cin >> item.value;
-            //push()
-            break; 
-            case 2:
-            //if func == pop"
-            //pop()
-            case 3:
+            cout << item.value;
+        } else if (func == 2)
+        {
+            Stack g;
+            cout <<"top is: "<< g.peek() << endl;
+        } else
+        {
             cout << "Not valid option, will exit";
             exit += 1;
         } 
-    } while (exit = 0);
-    
+        
+        // switch (func){
+        //     case 1: 
+        //         cout << "enter value: " << endl;
+        //         cin >> item.value;
+        //         //push()
+        //         break; 
+        //     case 2:
+        //         //pop()
+        //         break;
+        //     case 3:
+        //         Stack g;
+        //         cout << g.peek() << endl;
+        //         break;
+        //     case 4:
+        //         cout << "Not valid option, will exit";
+        //         exit += 1;
+        //         break;
 
-    initialNode.next = &item;
-    initialNode.next = &head;
-    head.next = &initialNode;
+        // } 
+    }
+
+
+    // initialNode.next = &item;
+    // initialNode.next = &head;
+    // head.next = &initialNode;
     
     //Memory deallocation
     //delete newNode;
     //IntNode* newNode = NULL;
-    return 0;
 }
 
--- Lab3_Grp14_struct.hpp
+++ Lab3_Grp14_struct.hpp
@@ -1,26 +1,42 @@
-typedef struct {
+using namespace std; 
+
+struct IntNode{
     int value;
     IntNode* next;
-} IntNode;
+};
 
+typedef struct {
+    IntNode* top=NULL;  
+} IntStack;
 
 class Stack {
 public:
-    typedef struct {
-        top=NULL;
-        newVal;  
-    } IntStack;
-
     //push method
-    void push(int value){
+    void push(int data){
         IntNode* newNode = new IntNode;
-        newNode->value = newVal;
+        newNode->value = data;
         newNode->next = top;
         top = newNode;
     }
 
     //pop method
     void pop(){
-
+        if (top == NULL) {
+            cout << "Stack is empty" << endl;
+        }
+        IntNode* temp = top;
+        top = top->next;
+        delete temp;
     }
-}
+
+    int peek() {
+        if (top == NULL) {
+            cout << "Stack is empty" << endl;
+            return -1;
+        }
+        return top->value;
+    }
+
+private:
+    IntNode* top;
+};
